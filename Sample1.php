d4430bdd00c6c826d1b4dbea720ea04cc3570180
Nopposan0123 <127659261+Nopposan0123@users.noreply.github.com>
2023-04-23 00:18:02 +0900

    Add files via upload

---
+++ Sample1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    echo('文字出力あるよ');
+
+    print('同様あるよ');
+
+    
+
+?>
+
+</body>
+<html>
